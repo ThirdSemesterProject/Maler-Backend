fbddd645a93fe341d76b521fd7c6a2df0cb94edc
ViggoBeck <vibe0002@stud.kea.dk>
2024-11-26 10:10:04 +0100

    Fixed products and order manytomany relation

--- src/main/java/stud/kea/dk/malerbackend/orders/model/Orders.java
+++ src/main/java/stud/kea/dk/malerbackend/orders/model/Orders.java
@@ -5,10 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import stud.kea.dk.malerbackend.customer.model.Customer;
-
+import stud.kea.dk.malerbackend.products.model.Products;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -19,17 +18,22 @@ public class Orders {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn()
-    Products products;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_products",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Products> products;
+
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn()
-    Customer customer;
-    String pickup;
-    double totalPrice;
-    LocalDate date;
+    @JoinColumn
+    private Customer customer;
+    private String pickup;
+    private double totalPrice;
+    private LocalDate date;
 
     public Orders() {
-
     }
 }
--- src/main/java/stud/kea/dk/malerbackend/products/model/Products.java
+++ src/main/java/stud/kea/dk/malerbackend/products/model/Products.java
@@ -1,18 +1,30 @@
 package stud.kea.dk.malerbackend.products.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import stud.kea.dk.malerbackend.orders.model.Orders;
 
+import java.util.List;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String name;
     private double pris;
     private String info;
     private String categories;
     private String brand;
 
+    @ManyToMany(mappedBy = "products")
+    private List<Orders> orders;
+
+    public Products() {
+    }
 }
