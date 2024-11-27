fae5aa8603bb8a5b6de9195d1abaf509b7b51faf
ViggoBeck <vibe0002@stud.kea.dk>
2024-11-27 10:37:32 +0100

    Made a few controller in the paintController.

--- src/main/java/stud/kea/dk/malerbackend/paint/controller/PaintController.java
+++ src/main/java/stud/kea/dk/malerbackend/paint/controller/PaintController.java
@@ -1,9 +1,45 @@
 package stud.kea.dk.malerbackend.paint.controller;
 
-import org.springframework.stereotype.Controller;
 
-@Controller
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import stud.kea.dk.malerbackend.paint.model.Paint;
+import stud.kea.dk.malerbackend.paint.repository.PaintRepository;
+import stud.kea.dk.malerbackend.paint.service.PaintService;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/paint")
+@CrossOrigin(origins = "http://localhost:8080")
 public class PaintController {
+
+    final private PaintService paintService;
+
+
+
+    public PaintController(PaintService paintService) {
+        this.paintService = paintService;
+    }
+    @GetMapping("/getAllPaints")
+    public List<Paint> getAllPaints() {
+        return paintService.getAllPaints();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Paint> getPaint(@PathVariable Long id) {
+        Paint paint = paintService.getPaintById(id);
+        if (paint != null) {
+            return ResponseEntity.ok(paint);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/makePaint")
+    public Paint makePaint(@RequestBody Paint paint) {
+        return paintService.createPaint(paint);
+    }
 
 
 }
