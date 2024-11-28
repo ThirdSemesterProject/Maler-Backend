7c9a8b0439260986593cac9ab0d3892b87d04d1c
Askethemervin <Aske.johanson@gmail.com>
2024-11-28 13:57:03 +0100

    implementation af søgefunktion

--- src/main/java/stud/kea/dk/malerbackend/paint/repository/PaintRepository.java
+++ src/main/java/stud/kea/dk/malerbackend/paint/repository/PaintRepository.java
@@ -1,6 +1,8 @@
 package stud.kea.dk.malerbackend.paint.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import stud.kea.dk.malerbackend.paint.model.Paint;
 
@@ -8,7 +10,8 @@ import java.util.List;
 
 @Repository
 public interface PaintRepository extends JpaRepository<Paint, Integer> {
-    List<Paint> findByNameContainingIgnoreCaseOrPaintNo_ItemNoContainingIgnoreCase(String name, String itemNo);
+    @Query("SELECT p FROM Paint p JOIN FETCH p.paintNo WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :query, '%')) OR LOWER(p.paintNo.itemNo) LIKE LOWER(CONCAT('%', :query, '%'))")
+    List<Paint> findByNameContainingIgnoreCaseOrPaintNo_ItemNoContainingIgnoreCase(@Param("query") String query);
 }
 
 
