4136d72a1ba00bf5fa168a0a4002cb6383e05d19
Martin Poulsen <mapo0004@stud.kea.dk>
2024-12-05 08:34:23 +0100

    Have added pagination to backend
    Have added pagination to backend for the admin site.

---
+++ src/main/java/stud/kea/dk/malerbackend/products/api/ProductsController.java
@@ -0,0 +1,65 @@
+package stud.kea.dk.malerbackend.products.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import stud.kea.dk.malerbackend.products.model.Products;
+import stud.kea.dk.malerbackend.products.service.ProductsService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@RequestMapping("api/products")
+@CrossOrigin
+@RestController
+public class ProductsController {
+
+    private final ProductsService productsService;
+
+    public ProductsController(ProductsService productsService) {
+        this.productsService = productsService;
+    }
+
+    @GetMapping("/getAllProducts")
+    public Page<Products> getAllProducts(Pageable pageable) {
+        return productsService.getAllProducts(pageable);
+    }
+
+    @GetMapping("/getBySubcategory")
+    public ResponseEntity<List<Products>> getProductsBySubcategory(@RequestParam String subcategory) {
+        try {
+            List<Products> products = productsService.getProductsBySubcategory(subcategory);
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Products> getPaint(@PathVariable Long id) {
+        Products products = productsService.getProductById(id);
+        if (products != null) {
+            return ResponseEntity.ok(products);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/createProduct")
+    public Products createProduct(@RequestBody Products products) {
+        return productsService.createProduct(products);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Products> updateProduct(@PathVariable long id, @RequestBody Products products) {
+        Products updateProducts = productsService.updateProduct(id, products);
+        return ResponseEntity.ok(updateProducts);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable long id) {
+        String result = productsService.deleteProductById(id);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/stud/kea/dk/malerbackend/products/service/ProductsService.java
@@ -0,0 +1,59 @@
+package stud.kea.dk.malerbackend.products.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+import stud.kea.dk.malerbackend.products.model.Products;
+import stud.kea.dk.malerbackend.products.repository.ProductsRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductsService {
+
+    private final ProductsRepository productsRepository;
+
+    public ProductsService(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+
+    public Products createProduct(Products products) {
+        return productsRepository.save(products);
+    }
+
+    public Page<Products> getAllProducts(Pageable pageable) {
+        Page<Products> products = productsRepository.findAll(pageable);
+        return products;
+    }
+
+    public List<Products> getProductsBySubcategory(String subcategory) {
+        return productsRepository.findBySubcategory(subcategory);
+    }
+
+    public Products getProductById(long id) {
+        return productsRepository.findById(id).orElse(null);
+    }
+
+    public Products updateProduct(long id, Products products) {
+        Optional<Products> productsOptional = productsRepository.findById(id);
+        if (productsOptional.isPresent()) {
+            Products productToUpdate = productsOptional.get();
+            productToUpdate.setName(products.getName());
+            productToUpdate.setDescription(products.getDescription());
+            productToUpdate.setPrice(products.getPrice());
+            productToUpdate.setCategory(products.getCategory());
+            productToUpdate.setSubcategory(products.getSubcategory());
+            productToUpdate.setBrand(products.getBrand());
+            return productsRepository.save(productToUpdate);
+        } else {
+            throw new EntityNotFoundException("Product with id " + id + " not found");
+        }
+    }
+
+    public String deleteProductById(long id) {
+        productsRepository.deleteById(id);
+        return "Product with id " + id + " deleted";
+    }
+}
